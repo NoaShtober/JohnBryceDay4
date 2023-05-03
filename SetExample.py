076f87378878a1c4eaa50bcafac58f861605ea41
NoaShtober <94970175+NoaShtober@users.noreply.github.com>
2023-05-03 16:35:17 +0300

    Create SetExample.py

---
+++ SetExample.py
@@ -0,0 +1,10 @@
+first_set = {"a", "b", "c", "d"}
+second_set = {"a", "c", "d", "f"}
+
+diff_set = first_set - second_set
+and_set = first_set & second_set
+or_set = first_set | second_set
+
+print(diff_set)
+print(and_set)
+print(or_set)
